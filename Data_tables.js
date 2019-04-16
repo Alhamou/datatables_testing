2177bf146d69f0a77938b7a743e5d190b2ebb1d0
Emad Alhamou <emadalhamo@gmail.com>
2019-04-16 08:53:25 +0200

    github config

---
+++ Data_tables.js
@@ -0,0 +1,25 @@
+class Data_tables {
+    constructor(arrays, form) {
+        this.arrays = arrays;
+        this.form = form;
+    }
+
+    table = $('#example').DataTable({
+        data: arrays,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
+        }
+    });
+
+    async add_row (){
+        await this.table.row.add( [
+            this.form.name.value,
+            this.form.position.value,
+            this.form.office.value,
+            this. form.age.value,
+            this.form.start_date.value,
+            this.form.salary.value
+        ]).draw( false );
+    }
+
+}
