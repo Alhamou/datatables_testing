faddc3b035e9d27ac73fcf38181b42f7c6380384
Emad Alhamou <emadalhamo@gmail.com>
2019-04-17 15:46:18 +0200

    added function getRow to Class

--- Data_table.js
+++ Data_table.js
@@ -7,15 +7,17 @@ class Data_table {
     
     // add new Row from [Form Object].
     async save (object){
-  
         await this.table.row.add(object).draw();
     }
-    delete (row){
+    delete (row) {
         this.table
                 .row(row )
                 .remove()
                 .draw();
               
+    }
+    getRow(row){
+       return this.table.row( row ).data();
     }
     
 }
