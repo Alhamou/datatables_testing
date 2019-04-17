bd98d5cfdb4f91ce1a2c2d2538cb8d36f195c91f
Emad Alhamou <emadalhamo@gmail.com>
2019-04-17 10:32:17 +0200

    added Edit and Show Row

--- Data_table.js
+++ Data_table.js
@@ -1,27 +1,15 @@
 class Data_table {
 
 
-    constructor() {}
+    constructor( ) {
 
 
+    }
+    
     // add new Row from [Form Object].
-    async add_row (form){
-
-        let arr = [];
-
-        // return validation erro Message for Client.
-        let validation_error = {error: '', message: ''};
-
-        // each Form Object.
-        for ( let i of form) {
-
-            ////////////////////////////////////////////////////////////////////
-            // add Code hear for validation the Data befor storage in Database//
-            ////////////////////////////////////////////////////////////////////
-
-            arr = [...arr, i.value];
-        }
-        await this.table.row.add(arr).draw();
+    async save (object){
+  
+        await this.table.row.add(object).draw();
     }
 
 }
